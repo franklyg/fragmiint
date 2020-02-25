584190fc70842f631ce2d0f9c504cbdede67a725
fgisonni <francesco@mindfulhealth.com>
2020-02-25 18:28:59 -0500

    Update for coming soon splash

---
+++ public/admin/cms.02615f5061d3875d1604.hot-update.js
@@ -0,0 +1,22 @@
+webpackHotUpdate("cms",{
+
+/***/ "./src/components/all.sass":
+/*!*********************************!*\
+  !*** ./src/components/all.sass ***!
+  \*********************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+// extracted by mini-css-extract-plugin
+    if(true) {
+      // 1582672472483
+      var cssReload = __webpack_require__(/*! ./node_modules/mini-css-extract-plugin/dist/hmr/hotModuleReplacement.js */ "./node_modules/mini-css-extract-plugin/dist/hmr/hotModuleReplacement.js")(module.i, {"hmr":true,"locals":false});
+      module.hot.dispose(cssReload);
+      module.hot.accept(undefined, cssReload);
+    }
+  
+
+/***/ })
+
+})
+//# sourceMappingURL=cms.02615f5061d3875d1604.hot-update.js.map
--- src/components/Layout.js
+++ src/components/Layout.js
@@ -48,9 +48,20 @@ const TemplateWrapper = ({ children }) => {
           content={`${withPrefix('/')}img/og-image.jpg`}
         />
       </Helmet>
-      <Navbar />
-      <div>{children}</div>
-      <Footer />
+      <section class="coming-soon-banner">
+        <img src="img/fragmiint.svg"/>
+        <div class="music-branding">
+          <a href="https://open.spotify.com/album/2rZxoCObBFfJ42Evzlznxs" class="spotify"><img src="https://cdn0.iconfinder.com/data/icons/picons-social/57/spotify_rounded-512.png" /></a>
+          <a href="https://music.apple.com/us/album/silent-methods-single/1480581567?ign-mpt=uo%3D4" class="apple"><img src="https://cdn3.iconfinder.com/data/icons/picons-social/57/16-apple-512.png" /></a>
+          <a href="https://play.google.com/store/music/album/Fragmiint_Silent_Methods?id=Bxavsydmhk3an6x7duvgigvzp3m" class="google"><img src="https://cdn0.iconfinder.com/data/icons/social-23/100/social_google_play-512.png" /></a>
+          <a href="https://www.youtube.com/watch?v=0tX4cZnbtwE&list=OLAK5uy_myJDRC3PRiELLgKrDeNBZTripFG73su2I" class="yt"><img src="https://cdn3.iconfinder.com/data/icons/social-media-black-white-2/512/BW_Youtube_glyph_svg-512.png" /></a>
+          <a href="https://tidal.com/browse/album/118273450" class="tidal"><img src="https://cdn4.iconfinder.com/data/icons/logos-brands-5/24/tidal-512.png" /></a>
+        </div>
+        <p>COMING SOON</p>
+      </section>
+      {/*<Navbar />*/}
+      {/*<div>{children}</div>*/}
+      {/*<Footer />*/}
     </div>
   )
 }
